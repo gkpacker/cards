7d9b4f90a763ab7998e76ffa36f5ad6c314de26f
Gabriel <ggkpacker@gmail.com>
2019-05-12 18:17:13 -0300

    Add create_hand and add tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,10 +1,11 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  Cards module handles a basic deck manipulation, where you can create a deck, shuffle it,
+  deal some amount of cards, save in a file and load it.
   """
 
   @doc """
-    Provides a list of strings representing deck cards.
+  Provides a list of strings representing deck cards.
 
   ## Examples
 
@@ -28,10 +29,37 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+  Verifies whether a deck contains a card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+      iex> Cards.contains?(deck, "Queen of Hearts")
+      false
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+  Deal specified amount of cards from given deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.deal(deck, 2)
+      {["Ace of Spades", "Ace of Diamonds"],
+       ["Ace of Hearts", "Ace of Clubs", "Two of Spades", "Two of Diamonds",
+       "Two of Hearts", "Two of Clubs", "Three of Spades", "Three of Diamonds",
+       "Three of Hearts", "Three of Clubs", "Four of Spades", "Four of Diamonds",
+       "Four of Hearts", "Four of Clubs", "Five of Spades", "Five of Diamonds",
+       "Five of Hearts", "Five of Clubs"]}
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -47,5 +75,14 @@ defmodule Cards do
       { :ok, binary } -> :erlang.binary_to_term(binary)
       { :error, _reason } -> "File '#{filename}' doesn't exists"
     end
+  end
+
+  @doc """
+  Creates a deck, shuffles it and deal how many cards specified as `hand_size`
+  """
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle
+    |> deal(hand_size)
   end
 end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test 'shuffle randomized the deck ordenation' do
+    deck = Cards.create_deck
+
+    refute Cards.shuffle(deck) == deck
+  end
+
+  test '' do
+
+  end
 end
